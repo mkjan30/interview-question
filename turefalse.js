0a7ce31f2f571dd3bf5ca683465116be0dddc202
Makshudul Karim <mkjan30@gmail.com>
2021-02-02 11:49:09 +0600

    truefalse

---
+++ turefalse.js
@@ -0,0 +1,15 @@
+// const name = 'Atif Aslam';
+//0
+//''
+//underfined
+//null
+//nan
+
+let name;
+console.log(name);
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
