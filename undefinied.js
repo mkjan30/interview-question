b53206451d6dfe398e16e46a86060a96a8cc98aa
Makshudul Karim <mkjan30@gmail.com>
2021-02-02 12:02:16 +0600

    undefined javascript

---
+++ undefinied.js
@@ -0,0 +1,20 @@
+// // let  pakhi;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(13, 82)
+// console.log(result);
+
+// function add (num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(13);
+// console.log(result);
+
+// const premik = {name: "smart dude", phone: 1524552};
+// console.log(premik.gf)
+
+let fun = undefined;
+console.log(fun);
+
